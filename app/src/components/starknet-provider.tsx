a6d494796877dc2d86ccfeacfd816dfcb24441e8
starkience <victor@sizaret.net>
2024-12-11 20:33:38 +0100

    test injectConnectors

--- app/src/components/starknet-provider.tsx
+++ app/src/components/starknet-provider.tsx
@@ -15,6 +15,7 @@ export function StarknetProvider({ children }: { children: React.ReactNode }) {
   const { connectors } = useInjectedConnectors({
     recommended: [argent(), braavos()],
     includeRecommended: "onlyIfNoConnectors",
+    order: "random",
   });
 
   return (
